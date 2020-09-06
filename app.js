ab178a368d9d05fb0301811f90be9c955e598ba6
anibalsmonteiro <anibalsmonteiro@gmail.com>
2020-09-06 11:41:20 -0300

    ajuste parametro

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://grades-app-asm.herokuapp.com/',
+    origin: 'https://grades-app-asm.herokuapp.com',
   })
 );
 
